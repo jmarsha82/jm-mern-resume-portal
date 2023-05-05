b7e544b6c8b972e778cf51b5d44fa97c215b390f
Justin Marshall <jmarsha82@yahoo.com>
2023-05-05 00:04:36 -0500

    Setting up class for button spacing

--- frontend/src/components/GeneralDetails.js
+++ frontend/src/components/GeneralDetails.js
@@ -25,7 +25,7 @@ const GeneralDetails = ({ general }) => {
       <p><PhoneAndroidIcon className='icon-text-alignment'/><strong>     {general.phone}</strong></p>
       <p><EmailIcon className='icon-text-alignment'/><strong>       {general.email}</strong></p>
       <p><strong>Last updated : </strong>{formatDistanceToNow(new Date(general.createdAt), { addSuffix: true})}</p>
-      <p><Button variant="contained" href="https://www.linkedin.com/in/justin-marshall-3733065b">LinkedIN</Button> <Button variant="contained" href="https://www.instagram.com/marshajus/">Instagram</Button></p>
+      <p><Button className='personal-site-buttons' variant="contained" href="https://www.linkedin.com/in/justin-marshall-3733065b">LinkedIN</Button> <Button variant="contained" href="https://www.instagram.com/marshajus/">Instagram</Button></p>
     </div>
   )
 }
