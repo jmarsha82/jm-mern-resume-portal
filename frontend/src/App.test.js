de7511e9237b1f5ab7754d3f94eb283bbdc877db
Justin Marshall <jmarsha82@yahoo.com>
2023-05-08 14:15:16 -0500

    Cleaned up test structure

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { shallow } from enzyme;
 import App from './App';
 
-describe('App', () => {
-  test('should render App component', () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper).toBeTruthy();
-    expect(wrapper.length).toBe(1);
-  })
+test('should render App component', () => {
+  const wrapper = shallow(<App/>);
+  expect(wrapper).toBeTruthy();
+  expect(wrapper.length).toBe(1);
 })
