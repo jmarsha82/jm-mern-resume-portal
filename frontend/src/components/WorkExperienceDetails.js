4f8274d0151730677f275ea9929b19ce8625e780
Justin Marshall <jmarsha82@yahoo.com>
2023-05-22 15:01:33 -0500

    Additional formatting added to experience

--- frontend/src/components/WorkExperienceDetails.js
+++ frontend/src/components/WorkExperienceDetails.js
@@ -4,19 +4,21 @@ const WorkExperienceDetails = ({ workExperience }) => {
   return (
     <div className="current-skill-details">
       <h4>{workExperience.company}</h4>
-      <div className="current-skill-details-desc">{workExperience.jobtitle}</div>
-      <div className="current-skill-details-desc">{workExperience.timerange}</div>
+      <div className="current-skill-details-desc">{workExperience.jobtitle}</div><br />
+      <div className="current-skill-details-desc">{workExperience.timerange}</div><br />
       <div className="current-skill-details-desc">{workExperience.location}</div>
-      <div className="current-skill-details-desc">{workExperience.commentone}</div>
-      <div className="current-skill-details-desc">{workExperience.commenttwo}</div>
-      <div className="current-skill-details-desc">{workExperience.commentthree}</div>
-      <div className="current-skill-details-desc">{workExperience.commentfour}</div>
-      <div className="current-skill-details-desc">{workExperience.commentfive}</div>
-      <div className="current-skill-details-desc">{workExperience.commentsix}</div>
-      <div className="current-skill-details-desc">{workExperience.commentseven}</div>
-      <div className="current-skill-details-desc">{workExperience.commenteight}</div>
-      <div className="current-skill-details-desc">{workExperience.commentnine}</div>
-      <div className="current-skill-details-desc">{workExperience.commentten}</div>
+      <ul className='work-experience-comment-list'>
+        <li><strong>{workExperience.commentone}</strong></li>
+        <li><strong>{workExperience.commenttwo}</strong></li>
+        <li><strong>{workExperience.commentthree}</strong></li>
+        <li><strong>{workExperience.commentfour}</strong></li>
+        <li><strong>{workExperience.commentfive}</strong></li>
+        <li><strong>{workExperience.commentsix}</strong></li>
+        <li><strong>{workExperience.commentseven}</strong></li>
+        <li><strong>{workExperience.commenteight}</strong></li>
+        <li><strong>{workExperience.commentnine}</strong></li>
+        <li><strong>{workExperience.commentten}</strong></li>
+      </ul>
     </div>
   )
 }
