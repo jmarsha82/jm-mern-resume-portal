27874b500116afadd01873c36fbc496019fc9752
Justin Marshall <jmarsha82@yahoo.com>
2023-05-25 15:04:00 -0500

    Updated formatting on work experience and education

--- frontend/src/components/EducationDetails.js
+++ frontend/src/components/EducationDetails.js
@@ -1,12 +1,28 @@
 import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { Link } from "react-router-dom";
 
 const EducationDetails = ({ education }) => {
   return (
-    <div className="education-skill-details">
-      <h4>{education.college}</h4>
-      <title>{education.location}</title>
-      <div className="education-title">{education.degree}</div>
-    </div>
+    // <div className="education-skill-details">
+    //   <h4>{education.college}</h4>
+    //   <title>{education.location}</title>
+    //   <div className="education-title">{education.degree}</div>
+    // </div>
+    <List component="nav" aria-label="mailbox folders">
+      <Divider />
+      <ListItem button component={Link} to={education.collegelink} target="_blank">
+        <ListItemText>
+          <h4>{education.college}</h4>
+          <title>{education.location}</title>
+          <div className="education-title">{education.degree}</div>
+        </ListItemText>
+      </ListItem>
+      <Divider />
+    </List>
   )
 }
 
