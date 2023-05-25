1f2105c623f84353779314b8e9edd74c18643d3f
Justin Marshall <jmarsha82@yahoo.com>
2023-05-25 17:51:10 -0500

    Added hover over on skill buttons

--- frontend/src/components/CurrentSkillDetails.js
+++ frontend/src/components/CurrentSkillDetails.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { Tooltip } from '@mui/material';
 
 const CurrentSkillDetails = ({ currentSkill }) => {
   return (
     <div className="current-skill-details">
-      <span><Button variant="contained" href={currentSkill.languagelink} target="_blank">{currentSkill.skillset}</Button></span>
+      <span>
+        <Tooltip title={"Used " + currentSkill.frequency} placement="top">
+          <Button variant="contained" href={currentSkill.languagelink} target="_blank">{currentSkill.skillset}</Button>
+        </Tooltip>
+        </span>
       <div className="current-skill-details-desc">{currentSkill.description}</div>
     </div>
   )
--- frontend/src/components/FullSkillDetails.js
+++ frontend/src/components/FullSkillDetails.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { Tooltip } from '@mui/material';
 
 const FullSkillDetails = ({ fullSkill }) => {
   return (
     <div className="current-skill-details">
-      <span><Button variant="contained" href={fullSkill.languagelink} target="_blank">{fullSkill.skillset}</Button></span>
+        <Tooltip title={"Used " + fullSkill.frequency} placement="top">
+          <Button variant="contained" href={fullSkill.languagelink} target="_blank">{fullSkill.skillset}</Button>
+        </Tooltip>
       <div className="current-skill-details-desc">{fullSkill.description}</div>
     </div>
   )
