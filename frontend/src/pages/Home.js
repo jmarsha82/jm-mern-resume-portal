fdd14d52c1e54bd4a03f8453b9a14d6c25ede177
Justin Marshall <jmarsha82@yahoo.com>
2023-06-08 17:39:56 -0500

    Updated background image

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,11 +5,7 @@ const Home = () => {
     <div>
       <div className="home-origin">
         <div className="choice-box">
-          <img className="choice-image" src={`${process.env.PUBLIC_URL}/img/art_background.jpg`} />
           <Button className="choice-button" variant="contained"  href="/artist">Artist</Button>
-        </div>
-        <div className="choice-box">
-          <img className="choice-image" src={`${process.env.PUBLIC_URL}/img/code_background.jpg`} />
           <Button className="choice-button" variant="contained"  href="/programmer">Developer</Button>
         </div>
       </div>
