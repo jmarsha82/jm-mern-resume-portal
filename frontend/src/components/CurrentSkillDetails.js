0cd8d1c4de51a4616c046076a64c026b1c925b5e
Justin Marshall <jmarsha82@yahoo.com>
2024-06-04 14:23:28 -0500

    Updated step one for removal of backend

--- frontend/src/components/CurrentSkillDetails.js
+++ frontend/src/components/CurrentSkillDetails.js
@@ -5,12 +5,25 @@ import { Tooltip } from '@mui/material';
 const CurrentSkillDetails = ({ currentSkill }) => {
   return (
     <div className="current-skill-details">
+      {/* <span>
+        <Tooltip title={"Used Daily"} placement="top">
+          <Button variant="contained" href={"https://azure.microsoft.com/en-us/products/devops/"} target="_blank">{"Azure Dev Ops"}</Button>
+        </Tooltip>
+        </span>
+      <div className="current-skill-details-desc">{"Used for Source Control, Work Tracking, Testing, CI/CD setup and monitoring, VM creation, etc."}</div> */}
       <span>
         <Tooltip title={"Used " + currentSkill.frequency} placement="top">
           <Button variant="contained" href={currentSkill.languagelink} target="_blank">{currentSkill.skillset}</Button>
         </Tooltip>
         </span>
       <div className="current-skill-details-desc">{currentSkill.description}</div>
+
+      {/* <span>
+        <Tooltip title={"Used " + currentSkill.frequency} placement="top">
+          <Button variant="contained" href={currentSkill.languagelink} target="_blank">{currentSkill.skillset}</Button>
+        </Tooltip>
+        </span>
+      <div className="current-skill-details-desc">{currentSkill.description}</div> */}
     </div>
   )
 }
--- frontend/src/components/GeneralDetails.js
+++ frontend/src/components/GeneralDetails.js
@@ -6,10 +6,10 @@ import EmailIcon from '@mui/icons-material/Email';
 const GeneralDetails = ({ general }) => {
   return (
     <div className="general-details">
-      <h4>Current Role : {general.position} at Boeing</h4>
-      <p><PhoneAndroidIcon className='icon-text-alignment'/><strong>     {general.phone}</strong></p>
-      <p><EmailIcon className='icon-text-alignment'/><strong>       {general.email}</strong></p>
-      <p><strong>Last updated : </strong>{formatDistanceToNow(new Date(general.createdAt), { addSuffix: true})}</p>
+      <h4>Current Role : Programmer at Boeing</h4>
+      <p><PhoneAndroidIcon className='icon-text-alignment'/><strong>     (618)-978-1469</strong></p>
+      <p><EmailIcon className='icon-text-alignment'/><strong>       jmarsha82@yahoo.com</strong></p>
+      <p><strong>Last updated : </strong>{formatDistanceToNow(new Date("2023-05-01T16:09:38.349+00:00"), { addSuffix: true})}</p>
     </div>
   )
 }
