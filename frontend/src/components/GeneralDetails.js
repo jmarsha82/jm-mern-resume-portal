3cb1b3a2f186e7af704514a0265854c002bd77b7
Justin Marshall <jmarsha82@yahoo.com>
2025-09-19 14:45:29 -0500

    Updated home page

--- frontend/src/components/GeneralDetails.js
+++ frontend/src/components/GeneralDetails.js
@@ -7,9 +7,9 @@ const GeneralDetails = ({ general }) => {
   return (
     <div className="general-details">
       <h4>Current Role : Programmer at Boeing</h4>
-      <p><PhoneAndroidIcon className='icon-text-alignment'/><strong>     (618)-978-1469</strong></p>
-      <p><EmailIcon className='icon-text-alignment'/><strong>       jmarsha82@yahoo.com</strong></p>
-      <p><strong>Last updated : </strong>{formatDistanceToNow(new Date("2023-05-01T16:09:38.349+00:00"), { addSuffix: true})}</p>
+      <p><PhoneAndroidIcon className='icon-text-alignment' /><strong>     (618)-978-1469</strong></p>
+      <p><EmailIcon className='icon-text-alignment' /><strong>       jmarsha82@yahoo.com</strong></p>
+      <p><strong>Last updated : </strong>{formatDistanceToNow(new Date("2025-09-19T16:09:38.349+00:00"), { addSuffix: true })}</p>
     </div>
   )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,44 @@
-import { Button } from "@mui/material";
+import { Button, Typography, Box, Avatar } from "@mui/material";
 import GeneralNavbar from "../components/GeneralNavbar";
 
 const Home = () => {
   return (
     <div>
       {/* <GeneralNavbar className="navbar-background" /> */}
-      <div className="home-origin">
-        <div className="choice-box">
-          <Button className="choice-button-artist" variant="contained"  href="/artist">Artist</Button>
-          <Button className="choice-button-dev" variant="contained"  href="/programmer">Developer</Button>
-        </div>
-      </div>
+
+      {/* Banner Section */}
+      <Box 
+        className="home-banner"
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/img/banner.jpg)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}
+      >
+        <Typography variant="h2" className="banner-title">
+          Justin Marshall
+        </Typography>
+        <Typography variant="h5" className="banner-subtitle">
+          Software Engineer and Artist
+        </Typography>
+      </Box>
+      
+      {/* Profile Picture */}
+      <Box className="profile-picture-container">
+        <Avatar
+          src={`${process.env.PUBLIC_URL}/img/profile_picture.jpg`}
+          alt="Justin Marshall"
+          className="profile-picture"
+        />
+      </Box>
+      
+       <div className="choice-box">
+         <Button className="choice-button-artist" variant="contained" href="/artist">Artist</Button>
+         <Button className="choice-button-dev" variant="contained" href="/programmer">Software Engineer</Button>
+       </div>
     </div>
-    
+
   )
 }
 
